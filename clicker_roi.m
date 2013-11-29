f77b0242f71a778d8b651bdd475b1d07fcb0bb0a
alex-attinger <a.attinger@gmail.com>
2013-11-29 18:51:06 +0100

    update on the cell selection. Click on rois you want to select

---
+++ clicker_roi.m
@@ -0,0 +1,28 @@
+function clicker_roi(h,~)
+   
+
+    %get(h, 'selectiontype')
+    % 'normal' for left moue button
+    % 'alt' for right mouse button
+    % 'extend' for middle mouse button
+    % 'open' on double click
+
+    a=floor(get(h, 'currentpoint'));
+    % Current mouse location, in pixels from the lower left.
+    % When the units of the figure are 'normalized', the
+    % coordinates will be [0 0] inb lower left, and [1 1] in
+    % the upper right.
+    a=a(1,:);
+   
+    s2 = [sprintf('current_roi = roi_nr(bm,%d,%d);',a(2),a(1))];
+    
+    evalin('base',s2);
+    
+    s3 = ['if ~isnan(current_roi); cell_rois(current_roi)=true;bm(:,:,current_roi) = false(400,750);i3 = imoverlay(im,bwperim(sum(bm,3)),[1 1 0]);axes(a);h=imagesc([1:750],[1:400],i3);set(a, ''ButtonDownFcn'', @clicker_roi);set(h,''HitTest'',''off'');end' ];
+    evalin('base',s3);
+    
+    
+    
+
+end
+
